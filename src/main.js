43a4dc815d107e96a21562b19812c13566c0e4ab
Peter <peter.olofsson@plockmatic.com>
2022-08-09 17:39:11 +0200

    Extracted AccountMenu

--- src/main.js
+++ src/main.js
@@ -37,3 +37,45 @@ const router = createRouter({
 app.use(router)
 app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
+
+
+// Ugly test from original, not working
+/*
+var userMenuDiv = document.getElementById("userMenu");
+var userMenu = document.getElementById("userButton");
+
+document.onclick = check;
+
+function check(e) {
+    var target = (e && e.target) || (event && event.srcElement);
+
+    //User Menu
+    if (!checkParent(target, userMenuDiv)) {
+        // click NOT on the menu
+        if (checkParent(target, userMenu)) {
+            // click on the link
+            if (userMenuDiv.classList.contains("invisible")) {
+                userMenuDiv.classList.remove("invisible");
+            } else {
+                userMenuDiv.classList.add("invisible");
+            }
+        } else {
+            // click both outside link and outside menu, hide menu
+            userMenuDiv.classList.add("invisible");
+        }
+    }
+
+}
+
+function checkParent(t, elm) {
+    while (t.parentNode) {
+        if (t == elm) {
+            return true;
+        }
+        t = t.parentNode;
+    }
+    return false;
+}
+*/
+
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <script setup>
 import ButtonRepo from "@/components/ButtonRepo.vue";
+import AccountMenu from "@/components/AccountMenu.vue";
 </script>
 
 <template>
@@ -41,7 +42,7 @@ import ButtonRepo from "@/components/ButtonRepo.vue";
 
             <!--<i class="fas fa-home fa-fw mr-3"></i>-->
             <!-- <font-awesome-icon icon="fa-solid fa-home" class="fas fa-fw mr-3"/> -->
-            <font-awesome-icon icon="fa-solid fa-home"/>
+            <font-awesome-icon icon="fa-solid fa-home" class="fa-fw mr-3"/>
             <span class="w-full inline-block pb-1 md:pb-0 text-sm">Home</span>
           </a>
         </li>
@@ -59,7 +60,7 @@ import ButtonRepo from "@/components/ButtonRepo.vue";
               hover:text-indigo-400
             "
           >
-            <font-awesome-icon icon="fa-solid fa-tasks"/>
+            <font-awesome-icon icon="fa-solid fa-tasks" class="fa-fw mr-3"/>
             <!-- <i class="fas fa-tasks fa-fw mr-3"></i> -->
             <span class="w-full inline-block pb-1 md:pb-0 text-sm">Tasks</span>
           </a>
@@ -78,7 +79,7 @@ import ButtonRepo from "@/components/ButtonRepo.vue";
               hover:text-indigo-400
             "
           >
-            <font-awesome-icon icon="fa-solid fa-envelope"/>
+            <font-awesome-icon icon="fa-solid fa-envelope" class="fa-fw mr-3"/>
             <!-- <i class="fa fa-envelope fa-fw mr-3"></i> -->
             <span class="w-full inline-block pb-1 md:pb-0 text-sm"
               >Messages</span
@@ -99,7 +100,7 @@ import ButtonRepo from "@/components/ButtonRepo.vue";
               hover:text-indigo-400
             "
           >
-            <font-awesome-icon icon="fa-solid fa-chart-area"/>
+            <font-awesome-icon icon="fa-solid fa-chart-area" class="fa-fw mr-3"/>
             <!-- <i class="fas fa-chart-area fa-fw mr-3 text-indigo-400"></i> -->
             <span class="w-full inline-block pb-1 md:pb-0 text-sm"
               >Analytics</span
@@ -120,7 +121,7 @@ import ButtonRepo from "@/components/ButtonRepo.vue";
               hover:text-indigo-400
             "
           >
-            <font-awesome-icon icon="fa-solid fa-wallet"/>
+            <font-awesome-icon icon="fa-solid fa-wallet" class="fa-fw mr-3"/>
             <!-- <i class="fa fa-wallet fa-fw mr-3"></i> -->
             <span class="w-full inline-block pb-1 md:pb-0 text-sm"
               >Payments</span
@@ -221,102 +222,7 @@ import ButtonRepo from "@/components/ButtonRepo.vue";
             <!--Menu-->
 
             <div class="flex relative inline-block pr-6">
-              <div class="relative text-sm">
-                <button
-                  id="userButton"
-                  class="flex items-center focus:outline-none mr-3"
-                >
-                  <img
-                    class="w-8 h-8 rounded-full mr-4"
-                    src="http://i.pravatar.cc/300"
-                    alt="Avatar of User"
-                  />
-                  <span class="hidden md:inline-block">Hi, User </span>
-                  <svg
-                    class="pl-2 h-2"
-                    version="1.1"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 129 129"
-                    xmlns:xlink="http://www.w3.org/1999/xlink"
-                    enable-background="new 0 0 129 129"
-                  >
-                    <g>
-                      <path
-                        d="m121.3,34.6c-1.6-1.6-4.2-1.6-5.8,0l-51,51.1-51.1-51.1c-1.6-1.6-4.2-1.6-5.8,0-1.6,1.6-1.6,4.2 0,5.8l53.9,53.9c0.8,0.8 1.8,1.2 2.9,1.2 1,0 2.1-0.4 2.9-1.2l53.9-53.9c1.7-1.6 1.7-4.2 0.1-5.8z"
-                      ></path>
-                    </g>
-                  </svg>
-                </button>
-                <div
-                  id="userMenu"
-                  class="
-                    bg-white
-                    nunito
-                    rounded
-                    shadow-md
-                    mt-2
-                    absolute
-                    mt-12
-                    top-0
-                    right-0
-                    min-w-full
-                    overflow-auto
-                    z-30
-                    invisible
-                  "
-                >
-                  <ul class="list-reset">
-                    <li>
-                      <a
-                        href="#"
-                        class="
-                          px-4
-                          py-2
-                          block
-                          text-gray-900
-                          hover:bg-indigo-400 hover:text-white
-                          no-underline
-                          hover:no-underline
-                        "
-                        >My account</a
-                      >
-                    </li>
-                    <li>
-                      <a
-                        href="#"
-                        class="
-                          px-4
-                          py-2
-                          block
-                          text-gray-900
-                          hover:bg-indigo-400 hover:text-white
-                          no-underline
-                          hover:no-underline
-                        "
-                        >Notifications</a
-                      >
-                    </li>
-                    <li>
-                      <hr class="border-t mx-2 border-gray-400" />
-                    </li>
-                    <li>
-                      <a
-                        href="#"
-                        class="
-                          px-4
-                          py-2
-                          block
-                          text-gray-900
-                          hover:bg-indigo-400 hover:text-white
-                          no-underline
-                          hover:no-underline
-                        "
-                        >Logout</a
-                      >
-                    </li>
-                  </ul>
-                </div>
-              </div>
+              <AccountMenu/>
             </div>
 
             <!-- / Menu -->
