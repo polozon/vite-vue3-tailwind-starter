c938545d23a68c2aaddcc6f259688701dc91760d
Peter Olofsson <peter.olofsson@plockmatic.se>
2022-08-10 12:56:08 +0200

    Used vue-click-outside, no progress

--- src/components/Menu/MenuButton.vue
+++ src/components/Menu/MenuButton.vue
@@ -1,5 +1,15 @@
 <template>
-    <button @click="$parent.toggle()">
+    <button @click="$parent.toggle()" v-click-outside="() => $parent.close()">
         <slot />
     </button>
 </template>
+
+<script>
+import ClickOutside from 'vue-click-outside'
+
+export default {
+  directives: {
+    ClickOutside
+  }
+};
+</script>
