c45c9367f568317debce18a543582fd7b43a5a2b
Peter Olofsson <peter.olofsson@plockmatic.se>
2022-08-10 12:35:41 +0200

    Added tailwind styling

--- src/components/Menu/MenuComponent.vue
+++ src/components/Menu/MenuComponent.vue
@@ -1,20 +1,20 @@
 <template>
     <div>
-        <Menu>
-            <MenuButton>
+        <Menu class="relative inline-block text-left">
+            <MenuButton class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
                 Options
             </MenuButton>
-            <MenuItems>
-                <MenuItem>
+            <MenuItems class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <MenuItem class="block px-4 py-2 text-sm">
                     Account Settings
                 </MenuItem>
-                <MenuItem>
+                <MenuItem class="block px-4 py-2 text-sm">
                     Support
                 </MenuItem>
-                <MenuItem>
+                <MenuItem class="block px-4 py-2 text-sm">
                     License
                 </MenuItem>
-                <MenuItem>
+                <MenuItem class="block px-4 py-2 text-sm">
                     Sign Out
                 </MenuItem>
             </MenuItems>
